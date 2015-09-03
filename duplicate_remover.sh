64b170d18ef10ea065c6810c76afe85cec34052d
Brad Richardson <brad.richardson@transware.net>
2015-09-02 19:48:06 -0500

    Fix looping for files with spaces

--- duplicate_remover.sh
+++ duplicate_remover.sh
@@ -2,6 +2,10 @@
 
 # A script for finding and removing duplicate files
 
+SAVEIFS=$IFS
+
+IFS=$(echo -e "\n")
+
 if [[ $# -ne 1 ]]
     then
     echo "usage: $0 directory_to_scan"
@@ -10,7 +14,11 @@ fi
 
 directory_to_scan="$1"
 
-for file in `find "$directory_to_scan" -name "*(1).*"`
+duplicate_names=`find "$directory_to_scan" -name "*(1).*"`
+
+for file in $duplicate_names
     do
     echo $file
 done
+
+IFS=$SAVEIFS
