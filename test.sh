750e48f357bc2ca87a3991aa8deb82f29f93e85e
Brad Richardson <brad.richardson@transware.net>
2015-09-02 20:24:17 -0500

    Fix test runner

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ touch diff_files\(1\).ext diff_files.ext file\(1\).ext \
     file1.ext file.ext new_file\(1\).ext new_file.ext \
     other\ file\(1\).ext other\ file.ext some_file\(1\).ext
 
+echo "stuff" > diff_files\(1\).ext
+
 duplicate_remover.sh ./
 
 files=`ls *.ext`
