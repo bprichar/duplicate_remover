74b67caeffe62e9733e437b79a3db45a0541fc9f
Brad Richardson <brad.richardson@transware.net>
2015-09-02 20:59:33 -0500

    Add some printing for status:

--- duplicate_remover.sh
+++ duplicate_remover.sh
@@ -15,12 +15,16 @@ fi
 directory_to_scan="$1"
 
 duplicate_names=`find "$directory_to_scan" -name "*(1).*"`
+potential_duplicates=`grep -c '(1)' <<< "$duplicate_names"`
 
+processed=0
 for file in $duplicate_names
     do
     original_name="${file/(1)/}"
     if diff "$original_name" "$file" &> /dev/null
         then
+        ((processed++))
+        echo "deleting '$file' $processed of $potential_duplicates"
         rm "$file"
     fi
 done
