c4f00fef9e96801675dd58afc571695dd19c4d67
Brad Richardson <brad.richardson@transware.net>
2015-09-02 20:16:35 -0500

    Create test runner

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+touch diff_files\(1\).ext diff_files.ext file\(1\).ext \
+    file1.ext file.ext new_file\(1\).ext new_file.ext \
+    other\ file\(1\).ext other\ file.ext some_file\(1\).ext
+
+duplicate_remover.sh ./
+
+files=`ls *.ext`
+expected='diff_files(1).ext diff_files.ext file1.ext file.ext new_file.ext other file.ext some_file(1).ext'
+
+if [[ $files == $expected ]]
+    then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
